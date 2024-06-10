69b6795a2da38316f7ff98481086c0114530bc5d
saekatanezaki <155140417+saekatanezaki@users.noreply.github.com>
2024-06-10 21:18:42 +0900

    詳細ページの実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,8 +1,9 @@
 class DiariesController < ApplicationController
+  before_action :set_diary, only: %i[edit update destroy show]
   before_action :set_selected_date, only: [:create_event]
 
   def index
-    @diaries = Diary.all
+    @diaries = Diary.order(created_at: :desc).all
     @selected_date = params[:selected_date]&.to_date || Date.today
   end
 
@@ -16,13 +17,27 @@ class DiariesController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    @diary.update(diary_params)
+    redirect_to root_path
+  end
+
   def destroy
-    diary = Diary.find(params[:id])
-    diary.destroy
+    @diary.destroy
     redirect_to root_path, notice: 'Diary was successfully deleted.'
   end
 
   private
+
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
 
   def diary_params
     params.require(:diary).permit(:date, :image, :title, :content)
